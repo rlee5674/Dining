670886811b0dae93f49a937df5e690c857558abd
Janny Huang <jannyhuang12@gmail.com>
2019-12-03 10:53:16 -0500

    added filtering by genre

--- display_categories.js
+++ display_categories.js
@@ -10,11 +10,33 @@ $(document).ready(function() {
     /* Display categories on page */
     categories.forEach(function(i, cat) {
         console.log(cat);
-        $("#categories").append('<li id="' + cat + '" class="category">' + cat + '</li>');
+        var c = $('<li id="' + cat + '" class="category">' + cat + '</li>');
+        c.bind('click', function() {
+            var f_recipes = filter_recipes(cat);
+            display_recipes(f_recipes);
+        });
+        $("#categories").append(c);
     });
 
     /* Display all recipes on page */
-    $.each(recipes, function(i, recipe) {
-
-    });
+    var f_recipes = filter_recipes("all");
+    display_recipes(f_recipes);
+    
 });
+
+function filter_recipes(genre) {
+    var filter_fun = (x) => {return x.Genre == genre};
+    if (genre == "all") {
+        filter_fun = (x) => {return true};
+    }
+    return recipes.filter(filter_fun);
+}
+
+function display_recipes(rs) {
+    $("#recipes").empty();
+    $.each(rs, function(i, recipe) {
+        var name = recipe.Title;
+        var r = $('<li id="' + name + '" class="category">' + name + '</li>');
+        $("#recipes").append(r);
+    })
+}
